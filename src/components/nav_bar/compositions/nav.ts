3074ecfa1b27c7e011506d1bcdd08c3cf079c7b9
Zeyan,Guo <403476891@qq.com>
2021-01-02 19:42:58 +0800

    refactor index page

--- src/components/nav_bar/compositions/nav.ts
+++ src/components/nav_bar/compositions/nav.ts
@@ -1,4 +1,4 @@
-import { reactive, onMounted } from 'vue'
+import { reactive, onMounted, computed, watch } from 'vue'
 import router from '@/router';
 
 interface Nav{
@@ -24,30 +24,58 @@ export default ():Nav => {
     icon: "icon-file-text-fill",
     title: "文章",
     navTo: "login"
+  },
+  {
+    icon: "icon-file-text-fill",
+    title: "文章超级无敌长哦哦哦",
+    navTo: "404"
   }];
   const navs:Array<NavItem> = reactive(navConfig);
-  const navsEl:Array<Element> = []
+  const navsEl:Array<any> = [];
+  
 
-  const m_setNavEl = (el:Element) =>{
+  const m_setNavEl = (el:any) =>{
     navsEl.push(el);
   }
 
-  const m_setActiveNav = () => {
-    const currRouteName = router.currentRoute.value.name;
+  // #region calculta border animate parameters.
+  const border = reactive({
+    scaleFactor: 1,
+    translateX: 0
+  });
 
-    navs.forEach(nav => {
+  const c_navBorder = computed(() => {
+    return {
+      transform: `translate(${border.translateX}px, 0px) scale(${border.scaleFactor}, 1)`
+    };
+  })
+
+  watch(router.currentRoute, (currRoute) => {
+    const currRouteName =  currRoute.name;
+    let translateX = 0, scaleFactor = 0, activeIndex = Number.MAX_SAFE_INTEGER;
+    navs.forEach((nav,index) => { //calculate the scaleFactor and translate x 
+      let clientRect = navsEl[index].$el.getBoundingClientRect();
+
       if(nav.navTo === currRouteName){
         nav.isActive = true;
+        
+        translateX += clientRect.width / 2;
+        scaleFactor = clientRect.width / 10;
+        activeIndex = index;
       }
       else{
         nav.isActive = false;
+        
+        if(activeIndex > index) translateX += clientRect.width;
       }
     });
-  }
+    border.scaleFactor = scaleFactor;
+    border.translateX = translateX;
+  })
+  //#endregion
 
   //lifecycle methods
   onMounted(() => {
-    m_setActiveNav();
   })
 
   return {
@@ -55,6 +83,9 @@ export default ():Nav => {
     navs,
 
     // methods
-    m_setNavEl
+    m_setNavEl,
+
+    //computed
+    c_navBorder
   }
 }
--- src/components/nav_bar/index.vue
+++ src/components/nav_bar/index.vue
@@ -2,33 +2,43 @@
   <section class="navbar">
     <header class="navbar_header">BLOG - 我爱吃烤鸡翅</header>
     <nav class="navbar_nav">
-      <router-link v-for="(item, index) in navs" :ref="m_setNavEl" :key="index" :to="item.navTo">
-        <span class="navbar_nav_icon iconfont" :class="item.icon"></span>
-        <span class="navbar_nav_name">{{item.title}}</span>
-      </router-link>
+      <div class="navbar_nav_container">
+        <router-link
+          v-for="(item, index) in navs"
+          :ref="m_setNavEl"
+          :key="index"
+          :to="item.navTo"
+          :class="{ navbar_nav_active: item.isActive }"
+        >
+          <span class="navbar_nav_icon iconfont" :class="item.icon"></span>
+          <span class="navbar_nav_name">{{ item.title }}</span>
+        </router-link>
+        <div class="navbar_nav_container_border" :style="c_navBorder"></div>
+      </div>
     </nav>
   </section>
 </template>
 <script lang="ts">
-import { defineComponent} from 'vue'
+import { defineComponent } from "vue";
 import useNav from "./compositions/nav";
 export default defineComponent({
-  setup(){
-    const {navs, m_setNavEl} = useNav();
+  setup() {
+    const { navs, m_setNavEl, c_navBorder } = useNav();
 
     return {
       navs,
-      m_setNavEl
-    }
-  }
+      m_setNavEl,
+      c_navBorder,
+    };
+  },
 });
 </script>
 <style lang="scss">
 @import "@/assets/scss/variable.scss";
 
-.navbar{
+.navbar {
   display: flex;
-  position:fixed;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
@@ -37,27 +47,52 @@ export default defineComponent({
   line-height: 0.38rem;
   box-shadow: 0px 3px 10px $gray-light;
   background: $bg;
-  &_header, &_nav{
+  &_header,
+  &_nav {
     flex: 1;
   }
-  &_header{
+  &_header {
     font-size: 0.22rem;
     font-weight: bold;
     padding-left: 0.2rem;
   }
-  &_nav{
+  &_nav {
     padding-right: 0.2rem;
-    text-align: right;
     font-size: 0.1rem;
-    &_name{
+    display: flex;
+    flex-direction: row-reverse;
+    &_container {
+      position: relative;
+      &_border {
+        content: "";
+        display: block;
+        position: absolute;
+        z-index: 10;
+        bottom: 0;
+        left: 0;
+        background: $black;
+        height: 0.02rem;
+        width: 10px;
+        transform-origin: 50% 0;
+        transition: transform 0.3s ease-in-out;
+      }
+    }
+    &_name {
       padding-left: 0.01rem;
     }
-    a{
+    &_active{
+      color: $black !important;
+    }
+    a {
+      display: inline-block;
       text-decoration: none;
-      color: $black;
+      color: $gray;
       padding: 0 0.05rem;
     }
-    span{
+    a:hover{
+      color: $black;
+    }
+    span {
       font-size: inherit;
     }
   }
